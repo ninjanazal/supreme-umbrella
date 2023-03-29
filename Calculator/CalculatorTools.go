a70292e74c8a9eb53a6f12ffd0bd4a1e8cadbf19
Eurico Martins <euricomartinss@gmail.com>
2023-03-30 00:22:47 +0100

    PostFix notation parser and stack implementation

--- Calculator/CalculatorTools.go
+++ Calculator/CalculatorTools.go
@@ -1,6 +1,10 @@
 package Calculator
 
-import "strings"
+import (
+	Opr "SupUmbrela/Utils"
+	Classes "SupUmbrela/Utils/Classes"
+	"strings"
+)
 
 // Minimize removes all spaces from the given string by replacing all spaces with an empty string.
 // @param data {*string} - A pointer to a string that needs to be minimized.
@@ -8,5 +12,47 @@ func Minimize(data *string) {
 	*data = strings.ReplaceAll(*data, " ", "")
 }
 
+// PostFix converts an infix expression to postfix notation.
+// @param data {*string} - A pointer to a string that represents the infix expression to convert.
+// @return {string} - A string that represents the postfix notation of the input expression.
 func PostFix(data *string) string {
+	var dataStack *Classes.Stack[byte] = Classes.NewStack[byte]()
+	var result strings.Builder = strings.Builder{}
+
+	for _, lookInChar := range *data {
+		switch lookInChar {
+		case '(':
+			dataStack.Push(byte(lookInChar))
+		case ')':
+			for dataStack.Peek() != '(' {
+				result.WriteByte(dataStack.Pop())
+				result.WriteByte(' ')
+			}
+			dataStack.Pop()
+		case '+', '-', '*', '/', '^':
+			result.WriteByte(byte(lookInChar))
+			result.WriteByte(' ')
+
+			for i := 1; i < len(*data); i++ {
+				nextChar := (*data)[1]
+				if nextChar == '(' {
+					dataStack.Push(nextChar)
+				} else if Opr.IsOperator(&nextChar) || nextChar == '-' {
+					result.WriteByte(byte(nextChar))
+					result.WriteByte(' ')
+					i++
+				} else {
+					break
+				}
+			}
+		default:
+			result.WriteString(string(lookInChar))
+			result.WriteByte(' ')
+		}
+	}
+	for !dataStack.IsEmpty() {
+		result.WriteByte(dataStack.Pop())
+		result.WriteByte(' ')
+	}
+	return result.String()
 }
--- Utils/Operations.go
+++ Utils/Operations.go
@@ -10,3 +10,10 @@ func Hash(s *string) uint32 {
 	h.Write([]byte(*s))
 	return h.Sum32()
 }
+
+// IsOperator checks if a given character is an arithmetic operator.
+// @param char {*byte} - A pointer to a `byte` that represents the character to check.
+// @return {bool} - A boolean value indicating whether the given character is an arithmetic operator.
+func IsOperator(char *byte) bool {
+	return (*char == '/' || *char == '*' || *char == '+' || *char == '-')
+}
